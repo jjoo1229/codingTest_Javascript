8613d91dfd469dac5e588d112b394ffca5ec8fbd
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2025-09-06 17:04:02 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.50 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/README.md
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 방금그곡 - 17683 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17683) 
 
 ### 성능 요약
 
-메모리: 34.8 MB, 시간: 7.06 ms
+메모리: 34.7 MB, 시간: 7.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 16:36:47
+2025년 09월 06일 17:04:02
 
 ### 문제 설명
 
--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -3,7 +3,7 @@ function solution(m, musicinfos) {
 
     for (let music of musicinfos) {
         let [start, finish, title, melody] = music.split(",")
-        let playtime = calculateTime(start, finish)  // 실제 재생된 시간
+        let playtime = calculateTime(start, finish)
         melody = switchMelody(melody)
         let musicLength = melody.length  // 음악의 길이
 
@@ -11,15 +11,10 @@ function solution(m, musicinfos) {
             let newMelody = melody.slice(0, playtime)
             map.set(title, newMelody)
         } else {  // 음악을 반복 재생해야 하는 경우
-            let newMelody = ""
-            for (let i = 0; i < Math.floor(playtime / musicLength); i++) {
-                newMelody += melody
-            }
-            newMelody += melody.slice(0, playtime % musicLength)
+            let newMelody = melody.repeat(Math.ceil(playtime / musicLength)).substr(0, playtime)
             map.set(title, newMelody)
         }
     }
-    console.log(map)
     let answer = []
 
     for (let [music, melody] of map.entries()) {
@@ -35,24 +30,14 @@ function solution(m, musicinfos) {
 }
 
 function switchMelody(melody) {
-    melody = melody.replace(/C#/g, "c")
-    melody = melody.replace(/D#/g, "d")
-    melody = melody.replace(/E#/g, "e")
-    melody = melody.replace(/F#/g, "f")
-    melody = melody.replace(/G#/g, "g")
-    melody = melody.replace(/A#/g, "a")
-    melody = melody.replace(/B#/g, "b")
-
-    return melody
+    return melody.replace(/(C#|D#|E#|F#|G#|A#|B#)/g, match => {
+        return match[0].toLowerCase()
+    })
 }
 
 function calculateTime(t1, t2) {
     let [h1, m1] = t1.split(":").map(Number)
     let [h2, m2] = t2.split(":").map(Number)
 
-    if (m2 >= m1) {
-        return (h2 - h1) * 60 + (m2 - m1)
-    } else {
-        return (h2 - h1 - 1) * 60 + (60 + m2 - m1)
-    }
+    return (h2 * 60 + m2) - (h1 * 60 + m1)
 }
