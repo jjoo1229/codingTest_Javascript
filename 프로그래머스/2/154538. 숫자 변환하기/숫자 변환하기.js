780f28f9cabfaba02f6cdabae3fcade34df84b0d
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2025-10-06 00:56:25 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.06 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,48 @@
+function solution(x, y, n) {
+    // y에서 시작해서 역으로 계산한다
+    const calc = (a, num) => {
+        switch(num) {
+            case 0:
+                return a - n;
+            case 1:
+                if(a % 2 === 0){
+                    return a / 2;
+                }
+                else{
+                    return 0;
+                }
+            case 2:
+                if(a % 3 === 0){
+                    return a / 3;
+                }
+                else{
+                    return 0;
+                }
+        }
+    }
+
+    const bfs = () => {
+        let queue = [[y, 0]];
+        let visit = {};
+        visit[y] = 1;
+
+        while (queue.length) {
+            let [cur, count] = queue.shift();
+
+            if(cur === x) return count;
+
+            for(let i = 0; i < 3; ++i){
+                let next = calc(cur,i);
+                if (next >= x && !visit[next]){
+                    visit[next] = 1;
+                    queue.push([next, count + 1]);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+
+    return bfs();
+}
