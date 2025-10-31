17fb4c5cf161d8436c009aa3ef1c9c521b633700
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2025-10-31 13:02:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 100 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.js
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.js
@@ -1,4 +1,4 @@
-// dfs 풀이
+// dp 풀이
 
 const input = require("fs")
     .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./baekjoon/input.txt")
@@ -13,34 +13,32 @@ for (let i = 1; i <= n; i++) {
     graph.push(input[i].split(" ").map(Number))
 }
 
-let answer = 0
-
+// dp[x][y][dir] 형식으로 저장할 것이기 때문에 3차원 배열 선언
 // dir은 0(가로), 1(세로), 2(대각선)
-const dfs = (x, y, dir) => {
-    if (x == n - 1 && y == n - 1) {
-        answer++
-        return
-    }
+let dp = Array.from({ length: n }, () => Array.from({ length: n }, () => Array(3).fill(0)))
 
-    // 가로 이동
-    if (dir == 0 || dir == 2) {
+// 초기 상태: (0, 1)에 가로 파이프(0)로 시작
+dp[0][1][0] = 1
+
+for (let x = 0; x < n; x++) {
+    for (let y = 0; y < n; y++) {
+        if (graph[x][y] == 1) continue
+
+        // 가로 이동
         if (y + 1 < n && graph[x][y + 1] == 0) {
-            dfs(x, y + 1, 0)
+            dp[x][y + 1][0] += dp[x][y][0] + dp[x][y][2]
         }
-    }
 
-    // 세로 이동
-    if (dir == 1 || dir == 2) {
+        // 세로 이동
         if (x + 1 < n && graph[x + 1][y] == 0) {
-            dfs(x + 1, y, 1)
+            dp[x + 1][y][1] += dp[x][y][1] + dp[x][y][2]
         }
-    }
 
-    // 대각선 이동
-    if (x + 1 < n && y + 1 < n && graph[x + 1][y] == 0 && graph[x][y + 1] == 0 && graph[x + 1][y + 1] == 0) {
-        dfs(x + 1, y + 1, 2)
+        // 대각선 이동
+        if (x + 1 < n && y + 1 < n && graph[x + 1][y] == 0 && graph[x][y + 1] == 0 && graph[x + 1][y + 1] == 0) {
+            dp[x + 1][y + 1][2] += dp[x][y][0] + dp[x][y][1] + dp[x][y][2]
+        }
     }
 }
-
-dfs(0, 1, 0) // (0, 1)에서 시작, 가로 방향
+let answer = dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]
 console.log(answer)
